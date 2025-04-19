3ccd7149d70704c5a2222aeb4fe488404f30e65b
Rahuldev Mondal <67589440+rdm123@users.noreply.github.com>
2025-04-19 12:17:18 +0530

    Update streamlit_app.py
    Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ if ingredients_list:
        
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json()) 
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 #Replace the code in this example app with your own code! And if you're new to Streamlit, here are some helpful links:  • :page_with_curl: [Streamlit open source documentation]({helpful_links[0]})
 #     • :snow: [Streamlit in Snowflake documentation]({helpful_links[1]}) 
 #     • :books: [Demo repo with templates]({helpful_links[2]})
